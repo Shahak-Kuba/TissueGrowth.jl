574960ba2ee33ddc14b867ed95d3f79c2fbd5a9f
Shaq <shahak.kuba@connect.qut.edu.au>
2024-01-18 08:08:42 +1000

    Added in solver for polar coords to simulate 2D shapes

--- src/Continuum/ContinuumSolver.jl
+++ src/Continuum/ContinuumSolver.jl
@@ -14,7 +14,7 @@ Tmax = 25.0
 Xmax = 2π
 
 
-function SolveContinuumLim1D(D,kf,A,ρ₀,growth_dir,Tmax,Xmax)
+function SolveContinuumLim_Cartesian(D,kf,A,ρ₀,growth_dir,Tmax,Xmax)
     # time and space discretisation
     T₀ = 0.0
     X₀ = 0.0
@@ -109,3 +109,95 @@ end
 
 
 
+function SolveContinuumLim_Polar(D,kf,A,ρ₀,growth_dir,Tmax)
+    # time and space discretisation
+    T₀ = 0.0
+
+    N = 3000
+    Δt = (Tmax - T₀)/N
+    t = LinRange(T₀,Tmax,Int64((Tmax - T₀)/Δt))
+
+    M = 200
+    Δθ = (2π)/M
+    θ = LinRange(0.0,2π,M)
+
+    # initialise all derivatives and variables
+    R = zeros(N+1,M)
+    Rθ = zeros(N+1,M)
+    Rθθ = zeros(N+1,M)
+    ρ = zeros(N+1,M)
+    ρθ = zeros(N+1,M)
+    κ = zeros(N+1,M)
+    λ = zeros(N+1,M)
+    Φ = zeros(N+1,M)
+    g = zeros(N+1,M)
+
+    # initialise matricies for cyclic tridiagonal
+    TriDiagMat = zeros(M,M)
+    d₀ = zeros(Float64,M)
+    d₁ = zeros(Float64,M-1)
+    d₋₁ = zeros(Float64,M-1)
+
+
+    # set initial conditions for h and ρ
+    R[1,:] = ones(size(θ)).*3.0 #2.0 .+ 0.5.*cos.(3*θ)
+    R[1,end] = R[1,1]
+    #x = 3 .* cos.(x)
+    ρ[1,:] = ones(1,M)*ρ₀
+
+    if growth_dir == "inward"
+        S = -1.0
+    else
+        S = 1.0
+    end
+
+    a⁺ = zeros(size(R[1,:]))
+    a⁻ = zeros(size(R[1,:]))
+    Rᵢ = zeros(size(R[1,:]))
+    Rᵢ₊₁ = zeros(size(R[1,:]))
+    Rᵢ₋₁ = zeros(size(R[1,:]))
+    ρᵢ = zeros(size(ρ[1,:]))
+    ρᵢ₊₁ = zeros(size(ρ[1,:]))
+    ρᵢ₋₁ = zeros(size(ρ[1,:]))
+    
+    @time for n in 1:N
+
+        Rᵢ .= R[n,:]
+        Rᵢ₊₁ .= circshift(Rᵢ,1)
+        Rᵢ₋₁ .= circshift(Rᵢ,-1)
+        ρᵢ = ρ[n,:]
+        ρᵢ₊₁ .= circshift(ρᵢ,1)
+        ρᵢ₋₁ .= circshift(ρᵢ,-1)
+
+        a⁺ .= aₘ₊(Rᵢ₊₁,Rᵢ₋₁)
+        a⁻ .= aₘ₋(Rᵢ₊₁,Rᵢ₋₁)
+
+        Rθ[n,:] .= (Rᵢ.*(a⁺.-a⁻) .- Rᵢ₋₁.*a⁺ .+ Rᵢ₊₁.*a⁻) ./ Δθ;                    # upwind to find hₓ,  refer eqn (26) in the notes
+        Rθθ[n,:] .= (Rᵢ₊₁ .- 2 .*Rᵢ .+ Rᵢ₋₁) ./ (Δθ^2);                               # cental to find hₓₓ, refer eqn (30) in the notes  
+        ρθ[n,:] .= (ρᵢ .* (a⁺.-a⁻) .- ρᵢ₋₁ .* a⁺ .+ ρᵢ₊₁ .* a⁻) ./ Δθ;
+        g[n,:] .= Rᵢ.*sqrt.(1 .+ (Rθ[n,:]./Rᵢ).^2)
+
+        κ[n,:] = (Rᵢ.^2 .- Rᵢ.*Rθθ[n,:] .+ 2 .* Rθ[n,:])./(g[n,:].^3)
+
+        Φ[n,:] = ρᵢ .- S.*Δt.*(ρᵢ.^2).*kf.*κ[n,:] .- (Δt.*ρθ[n,:].*Rθ[n,:].*kf.*ρᵢ) ./ (Rᵢ.*g[n,:]) .- ((D.*Δt.*ρθ[n,:].*Rθ[n,:]) ./ (Rᵢ.*g[n,:])).*(2 ./ g[n,:] .- κ[n,:]) .- A.*ρᵢ
+        λ[n,:] = (D.*Δt./(Δθ.^2)) ./ (g[n,:].^2)
+
+        R[n+1,:] = Rᵢ .+ S.*Δt.*kf.*ρᵢ.*(g[n,:]./Rᵢ)
+
+        # cyclic tridiagonal matrix
+        d₀ .= 1 .+ 2 .* λ[n,1:M]
+        d₁ .= -λ[n,1:M-1]
+        d₋₁ .= -λ[n,2:M]
+    
+        TriDiagMat .= diagm(-1 => d₋₁, 0 => d₀, 1 => d₁)
+        TriDiagMat[M,1] = -λ[n,M]
+        TriDiagMat[1,M] = -λ[n,1]
+
+        # solving the system
+        ρ[n+1,1:M] .= TriDiagMat\Φ[n,1:M]
+
+    end
+
+    return θ,R,ρ
+
+end
--- src/Continuum/main.jl
+++ src/Continuum/main.jl
@@ -3,7 +3,7 @@ using Plots
 include("ContinuumSolver.jl")
 
 # simulation parameters
-D = 0.01;
+D = 0.0001;
 kf = 0.002
 A = 0.0;
 ρ₀ = 22.918311805232928;
@@ -13,12 +13,18 @@ Xmax = 2π
 
 x,h,ρ = SolveContinuumLim1D(D,kf,A,ρ₀,growth_dir,Tmax,Xmax);
 
+θ,R,ρ = SolveContinuumLim_Polar(D,kf,A,ρ₀,growth_dir,Tmax)
+
 plot(x,h[1,:])
 plot!(x,h[500,:])
 plot!(x,h[1000,:])
 plot!(x,h[1500,:])
 plot!(x,h[2000,:])
 plot!(x,h[2500,:])
+
+plot(R[1,:].*cos.(θ),R[1,:].*sin.(θ))
+plot!(R[500,:].*cos.(θ),R[500,:].*sin.(θ))
+plot!(R[2500,:].*cos.(θ),R[2500,:].*sin.(θ))
 
 plot(x,ρ[1,1:end])
 plot!(x,ρ[500,1:end])
