44ee6096e72497348a0b378a2215f4f33a7f62c6
Shaq <n9996095@qut.edu.au>
2024-01-31 10:51:41 +1000

    multiple springs

--- Run/Comparison_Sims/ComparisonPlottingFncs.jl
+++ Run/Comparison_Sims/ComparisonPlottingFncs.jl
@@ -2,7 +2,7 @@ using CairoMakie
 using ColorSchemes
 using Colors
 
-function DiscVSContDensity_plot(Discrete_Solution, Continuum_Solution, index, btype)
+function DiscVSContDensity_plot(Discrete_Solution_m1, m1, Discrete_Solution_m2, m2, Continuum_Solution, index, btype)
     if index > 11
         error("index is too large!")
     end
@@ -10,15 +10,54 @@ function DiscVSContDensity_plot(Discrete_Solution, Continuum_Solution, index, bt
     θ_cont, R_cont, ρ_cont =  Continuum_Solution;
     
     # Getting Data Discrete
+    θ_disc1, R_disc1, ρ_disc1 = Convert_Discrete_Data(Discrete_Solution_m1, m1)
+    θ_disc2, R_disc2, ρ_disc2 = Convert_Discrete_Data(Discrete_Solution_m2, m2)
+    # sorting discrete solution values
+
+    min_y = floor(ρ_cont[1,1]; sigdigits=1)
+    max_y = ceil(maximum(ρ_cont[end,:]); sigdigits=1)
+
+    
+    ## Making Figure
+    txtSize = 35;
+    tickSize = 25;
+    plot_font = "Arial"
+    f = Figure(backgroundcolor=RGBf(0.98, 0.98, 0.98),
+        size=(1000, 800))
+    ga = f[1, 1] = GridLayout()
+    gaxmain = Axis(ga[1, 1], limits=(0, 2π, min_y, max_y), xticklabelsize = tickSize, yticklabelsize = tickSize, 
+                    xlabel="θ", xlabelsize=txtSize, xlabelfont = plot_font,
+                    ylabel="ρ", ylabelsize=txtSize,  ylabelfont = plot_font,
+                    title = "Pore: $btype, t = $(Discrete_Solution.t[index]) Simulation days", titlesize = txtSize, titlefont = plot_font)
+    
+    # plotting Continuum
+    cont_index = 1 + (index - 1)*1000
+    cont_line = CairoMakie.lines!(gaxmain, θ_cont, ρ_cont[cont_index,:], linewidth=5, color=:red)
+
+
+    # plotting Discrete
+    disc_index = index;
+    disc_scat1 = CairoMakie.stairs!(gaxmain, θ_disc1[disc_index,:], ρ_disc1[disc_index,:], step=:pre, linewidth=3, color=:blue)
+    disc_scat2 = CairoMakie.stairs!(gaxmain, θ_disc2[disc_index,:], ρ_disc2[disc_index,:], step=:pre, linewidth=3, color=:black)
+    #Legend(f[1,2], [cont_line, disc_scat1, disc_scat2], ["Continuum", "Discrete m = $m1", "Discrete m = $m2"])
+    axislegend(gaxmain,[cont_line, disc_scat1, disc_scat2], ["Continuum", "Discrete m = $m1", "Discrete m = $m2"],position=:rt)
+    return f
+end
+
+function Convert_Discrete_Data(Discrete_Solution, m)
+    # Getting Data Discrete
     x = zeros(size(Discrete_Solution.u,1),size(Discrete_Solution.u[1],1))
     y = zeros(size(x))
+    temp_r = zeros(size(x))
     temp_θ = zeros(size(x))
     temp_ρ = zeros(size(x))
     θ_disc = zeros(size(x))
     ρ_disc = zeros(size(x))
+    r_disc = zeros(size(x))
     for i in axes(x,1)
         x[i,:] .= Discrete_Solution.u[i][:,1];
         y[i,:] .= Discrete_Solution.u[i][:,2];
+        temp_r[i,:] .= sqrt.(x[i,:].^2 .+ y[i,:].^2)
         temp_θ[i,:] .= atan.(y[i,:],x[i,:]);
         for j in axes(temp_θ,2)
             if temp_θ[i,j] < 0
@@ -30,32 +69,9 @@ function DiscVSContDensity_plot(Discrete_Solution, Continuum_Solution, index, bt
         θ_disc[i,:] = [temp_θ[i, θmin_idx:end]; temp_θ[i, 1:θmin_idx-1]]
 
         temp_ρ[i,:] = Discrete_Solution.Density[i].data
-        ρ_disc[i,:] = [temp_ρ[i, θmin_idx:end]; temp_ρ[i, 1:θmin_idx-1]]
+        ρ_disc[i,:] = [temp_ρ[i, θmin_idx:end]; temp_ρ[i, 1:θmin_idx-1]]./m
+        r_disc[i,:] = [temp_r[i, θmin_idx:end]; temp_r[i, 1:θmin_idx-1]]
     end
-    # sorting discrete solution values
 
-    
-    ## Making Figure
-    txtSize = 35;
-    tickSize = 25;
-    plot_font = "Arial"
-    f = Figure(backgroundcolor=RGBf(0.98, 0.98, 0.98),
-        size=(1000, 800))
-    ga = f[1, 1] = GridLayout()
-    gaxmain = Axis(ga[1, 1], limits=(0, 2π, 22, 60), xticklabelsize = tickSize, yticklabelsize = tickSize, 
-                    xlabel="θ", xlabelsize=txtSize, xlabelfont = plot_font,
-                    ylabel="ρ", ylabelsize=txtSize,  ylabelfont = plot_font,
-                    title = "Pore: $btype, t = $(Discrete_Solution.t[index]) simulation days", titlesize = txtSize, titlefont = plot_font)
-    
-    # plotting Continuum
-    cont_index = 1 + (index - 1)*1000
-    cont_line = CairoMakie.lines!(gaxmain, θ_cont, ρ_cont[cont_index,:], linewidth=5, color=:red)
-
-
-    # plotting Discrete
-    disc_index = index;
-    disc_scat = CairoMakie.stairs!(gaxmain, θ_disc[disc_index,:], ρ_disc[disc_index,:], step=:pre, linewidth=3, color=:black)
-    Legend(f[1,2], [cont_line, disc_scat], ["Continuum", "Discrete m = 1"])
-
-    return f
+    return θ_disc,r_disc,ρ_disc
 end
--- Run/Comparison_Sims/Discrete_VS_Continuous.jl
+++ Run/Comparison_Sims/Discrete_VS_Continuous.jl
@@ -4,18 +4,18 @@ include("ComparisonPlottingFncs.jl")
 # Shared variables
 R₀ = 1.05  
 D = 0.005
-kf = 0.0005
+kf = 0.001
 growth_dir = "inward"
 Tmax = 26.0 # days
-btype = "hex" #Options: ["circle", "triangle", "square", "hex", "star","cross"]
+btype = "square" #Options: ["circle", "triangle", "square", "hex", "star","cross"]
 
 # Discrete Simulation
 # set random seed number for reproducability 
 seed = 88
 
 # setting up simulation parameters
-N = 180 # number of cells
-m = 1 # number of springs per cell
+N = 96 # number of cells
+m1 = 1 # number of springs per cell
 l₀ = 1.0
 η = 1.0 
 δt = 0.01
@@ -23,24 +23,29 @@ dist_type = "Linear" #Options: ["Linear", "sigmoid", "2sigmoid", "exp",  "sine",
 
 ## Cell Behaviours
 prolif = false; death = false; embed = false;
-α = 0.0001;        β = 0.001;      γ = 0.01;
+α = 0.0001;        βv = 0.001;      γv = 0.01;
 event_δt = δt
 
 # 2D simulations 
-sols2D = TissueGrowth.sim2D(N,m,R₀,D,l₀,kf,η,growth_dir,Tmax,δt,[btype],dist_type,
-            prolif, death, embed, α, β, γ, event_δt, seed, 11);
+sols2D_m1 = TissueGrowth.sim2D(N,m1,R₀,D,l₀,kf,η,growth_dir,Tmax,δt,[btype],dist_type,
+            prolif, death, embed, α, βv, γv, event_δt, seed, 11);
+
+m2 = 2 # number of springs per cell
+sols2D_m2 = TissueGrowth.sim2D(N,m2,R₀,D,l₀,kf,η,growth_dir,Tmax,δt,[btype],dist_type,
+            prolif, death, embed, α, βv, γv, event_δt, seed, 11);
 
 
 # Continuum Simulation
-A = 0.00;
-ρ₀ = sols2D[1][1].Density[1][1];
+Av = 0.00;
+ρ₀ = sols2D_m1[1][1].Density[1][1];
 
-θ_cont,R_cont,ρ_cont = TissueGrowth.SolveContinuumLim_Polar(D,kf,A,ρ₀,Tmax,R₀,btype,growth_dir);
+θ_cont,R_cont,ρ_cont = TissueGrowth.SolveContinuumLim_Polar(D,kf,Av,ρ₀,Tmax,R₀,btype,growth_dir);
 
 
 
 # plotting
-Discrete_Solution = sols2D[1][1];
+Discrete_Solution_m1 = sols2D_m1[1][1];
+Discrete_Solution_m2 = sols2D_m2[1][1];
 Continuum_Solution = (θ_cont,R_cont,ρ_cont);
-index = 11
-DiscVSContDensity_plot(Discrete_Solution, Continuum_Solution, index, btype)
+index = 9
+DiscVSContDensity_plot(Discrete_Solution_m1, m1, Discrete_Solution_m2, m2, Continuum_Solution, index, btype)
