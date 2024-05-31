5923280aa110f701c9cacae2f82ec668ad5a6ba8
ShahakMac <shahakuba98@gmail.com>
2024-05-31 11:28:28 +1000

    Added data export code in misc.jl

--- src/Discrete/Misc.jl
+++ src/Discrete/Misc.jl
@@ -5,59 +5,40 @@ function printInfo(simNum,simTotal,btype,N,kₛ,η,kf,M,D)
     println(@sprintf "-----------------------------------------------------------------------------------")
 end
 
-function export_figure(fig, image_name, image_type)
-    # Get the current directory
-    current_dir = pwd()
+# Helper function to convert ElasticMatrix and ElasticVector to regular Matrix and Vector
+function convert_to_regular_matrix_vector(sim_results::SimResults_t)
+    u_x_positions = [el[:, 1].data for el in sim_results.u]
+    u_y_positions = [el[:, 2].data for el in sim_results.u]
+    ∑F_vectors = [el.data for el in sim_results.∑F]
+    Density_matrices = [el.data for el in sim_results.Density]
+    ψ_vectors = [el.data for el in sim_results.ψ]
+    Κ_matrices = [el.data for el in sim_results.Κ]
     
-    # Generate the save directory path
-    save_dir = joinpath(current_dir, "figures")
-
-    # Check if save directory exists, create it if it doesn't
-    if !isdir(save_dir)
-        mkdir(save_dir)
-    end
-    
-    # Generate the timestamp
-    timestamp = Dates.now()
-    timestamp_str = Dates.format(timestamp, "yyyymmdd")
-    
-    # Generate the filename
-    filename = joinpath(save_dir, "$image_name-$timestamp_str.$image_type")
-    
-    # Save the figure
-    save(filename, fig)
+    return (u_x_positions, u_y_positions, ∑F_vectors, Density_matrices, ψ_vectors, Κ_matrices)
 end
 
-function SaveData(data, SaveName, SaveFolder)
-    # Check if the folder exists, create it if it doesn't
-    if !isdir(SaveFolder)
-        mkdir(SaveFolder)
-    end
-    
-    # Construct the file path
-    filepath = joinpath(SaveFolder, "$SaveName.jld2")
-    
-    # Check if the file already exists
-    if isfile(filepath)
-        error("File '$SaveName.jld2' already exists in folder '$SaveFolder'. Please choose a different name.")
-    else
-        # Save the data to the specified file
-        save(filepath, "data", data)
-    end
+# Function to convert a vector of vectors to a DataFrame column of vectors
+function vector_of_vectors_to_column(vov::Vector{Vector{T}}) where T
+    return vov
 end
 
-function LoadData(SaveName, SaveFolder)
-    # Construct the file path
-    filepath = joinpath(SaveFolder, "$SaveName.jld2")
-    
-    # Load the data from the specified file
-    loaded_data = load(filepath)
-    
-    # Retrieve the data from the loaded file
-    if haskey(loaded_data, "data")
-        return loaded_data["data"]
-    else
-        error("No 'data' key found in the loaded file.")
-    end
-end
+# Function to export the data to CSV
+function export_data_to_csv(sim_results::SimResults_t, filename::String)
+    u_x_positions, u_y_positions, ∑F_vectors, Density_matrices, ψ_vectors, Κ_matrices = convert_to_regular_matrix_vector(sim_results)
 
+    # Convert vectors of vectors to DataFrame columns
+    df = DataFrame(
+        t = sim_results.t,
+        pos_x = vector_of_vectors_to_column(u_x_positions),
+        pos_y = vector_of_vectors_to_column(u_y_positions),
+        Density = vector_of_vectors_to_column(Density_matrices),
+        Stress = vector_of_vectors_to_column(ψ_vectors),
+        Force_Sum = vector_of_vectors_to_column(∑F_vectors),
+        Velocity = vector_of_vectors_to_column(sim_results.Vₙ),
+        Void_Area = sim_results.Ω,
+        Curvature = vector_of_vectors_to_column(Κ_matrices),
+        Cell_Count = sim_results.CellCount
+    )
+    
+    CSV.write(filename, df)
+end
